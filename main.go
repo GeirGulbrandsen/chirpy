e4e6c298b7c156f1a8f2bf4c7f07eac552f9200c
Geir Gulbrandsen <4066559+GeirGulbrandsen@users.noreply.github.com>
2026-03-30 20:26:20 +0100

    Counting and resetting

--- main.go
+++ main.go
@@ -1,20 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write([]byte("Hits: 0"))
+}
+
 func main() {
+	apiCfg := &apiConfig{}
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write([]byte(http.StatusText(http.StatusOK)))
 	})
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	fileserver := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app", fileserver))
+	handler := http.StripPrefix("/app", fileserver)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
