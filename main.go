7721352793547d07cdeda1bde7c9697b1a3060e7
Geir Gulbrandsen <4066559+GeirGulbrandsen@users.noreply.github.com>
2026-03-30 20:14:55 +0100

    We have a heartbeat

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte(http.StatusText(http.StatusOK)))
+	})
+
+	fileserver := http.FileServer(http.Dir("."))
+	mux.Handle("/app/", http.StripPrefix("/app", fileserver))
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
